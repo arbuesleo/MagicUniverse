767cdf0a8614d4b5153c8e7f211400b8942fabdd
Leonardo Gomes <leogomes26@hotmail.com>
2016-12-04 15:48:11 -0200

    Start2

---
+++ src/main/java/com/leonardo/gomes/MagicUniverseApplication.java
@@ -0,0 +1,12 @@
+package com.leonardo.gomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicUniverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicUniverseApplication.class, args);
+	}
+}
